d504c346aedcc8835c1d49d0bb3ce838d1cff755
rraiatsysgain <rrai@sysgain.com>
2017-12-26 21:48:18 -0800

    Create init.sh

---
+++ oracle-oci/init.sh
@@ -0,0 +1,12 @@
+ls
+pwd
+echo "Initializing terraform..."
+terraform init \
+-input=false \
+-backend-config="bucket=$TF_VAR_terraform_remote_bucket" \
+-backend-config="key=$TF_VAR_deployment_auuid/terraform.tfstate" \
+-backend-config="region=$TF_VAR_terraform_remote_bucket_region" \ 
+-backend-config="encrypt=true" \
+-backend-config="access_key=$TF_VAR_aws_access_key" \
+-backend-config="secret_key=$TF_VAR_aws_secret_key" \
+-force-copy 
