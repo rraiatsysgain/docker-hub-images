b9c4f2dd627dbbfb3002163927840f119bce5371
rraiatsysgain <rrai@sysgain.com>
2018-03-21 15:09:12 -0700

    updated install script

--- azure-cli/install.sh
+++ azure-cli/install.sh
@@ -2,7 +2,7 @@
 wget $TEMPLATE_URL -O soln.zip
 
 unzip soln.zip
-#wget $PRIVATE_KEY_URL -O bmcs_api_key.pem
+wget $AZ_VAR_app_secret -O app_key.pem
 #v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
 #v="$v."
 #cp -r $v .
