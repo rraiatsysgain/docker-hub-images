ec4b816817490c2f2f3f80726780c61cc418ce69
rraiatsysgain <rrai@sysgain.com>
2018-04-11 13:37:51 -0700

    updated install.sh

--- terraform-provider/install.sh
+++ terraform-provider/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -o soln.zip $TEMPLATE_URL
 #curl -o bmcs_api_key.pem $PRIVATE_KEY_URL
-v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
-v="$v."
-cp -r $v .
+#v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
+#v="$v."
+#cp -r $v .
+unzip soln.zip
