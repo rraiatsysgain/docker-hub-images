c4ec04564913377be94b01c9667f79159cb8c5d9
rraiatsysgain <rrai@sysgain.com>
2018-04-13 00:29:47 -0700

    reverted parameter file

--- azure-cli/parameter.sh
+++ azure-cli/parameter.sh
@@ -1,12 +1,12 @@
-env > env.txt
-json="{\"\$schema\": \"http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#\",\"contentVersion\": \"1.0.0.0\",\"parameters\": {"
-while read p; do
-    key=$(echo $p | cut -d'=' -f 1)
-    value=$(echo $p | cut -d'=' -f 2)
-    json=$json"\"$key\":{\"value\":\"$value\"},"
-done < env.txt
-json=$json"}}"
+#env > env.txt
+#json="{\"\$schema\": \"http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#\",\"contentVersion\": \"1.0.0.0\",\"parameters\": {"
+#while read p; do
+#    key=$(echo $p | cut -d'=' -f 1)
+ #   value=$(echo $p | cut -d'=' -f 2)
+  #  json=$json"\"$key\":{\"value\":\"$value\"},"
+#done < env.txt
+#json=$json"}}"
 
-cat <<EOF > azuredeploy.parameters.json
-$json
-EOF
+#cat <<EOF > azuredeploy.parameters.json
+#$json
+#EOF
