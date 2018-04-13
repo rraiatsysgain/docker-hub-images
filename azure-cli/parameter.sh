6a4d299146c804e7d56dabff22aac51f3a206ba0
rraiatsysgain <rrai@sysgain.com>
2018-04-12 19:27:44 -0700

    Fixed issue with parameter file

--- azure-cli/parameter.sh
+++ azure-cli/parameter.sh
@@ -1,8 +1,10 @@
-unset IFS
+env > env.txt
 json="{\"\$schema\": \"http://schema.management.azure.com/schemas/2015-01-01/deploymentParameters.json#\",\"contentVersion\": \"1.0.0.0\",\"parameters\": {"
-for var in $(compgen -e); do
-    json=$json"\"$var\":{\"value\":\"${!var}\"},"
-done
+while read p; do
+    key=$(echo $p | cut -d'=' -f 1)
+    value=$(echo $p | cut -d'=' -f 2)
+    json=$json"\"$key\":{\"value\":\"$value\"},"
+done < env.txt
 json=$json"}}"
 
 cat <<EOF > azuredeploy.parameters.json
