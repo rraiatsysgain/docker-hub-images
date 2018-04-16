3eca6d4c8ad7fcfae77d8512191a9d287f7b6125
rraiatsysgain <rrai@sysgain.com>
2018-04-16 12:01:05 -0700

    fixed terraform

--- aws-terraform/install.sh
+++ aws-terraform/install.sh
@@ -2,10 +2,11 @@
 curl -o soln.zip $TEMPLATE_URL
 curl -o access_key.pem $TF_VAR_AWS_SECRET_ACCESS_KEY
 
-export AWS_ACCESS_KEY_ID=$TF_VAR_AWS_ACCESS_KEY_ID
-export AWS_SECRET_ACCESS_KEY=$(cat access_key.pem)
-export AWS_DEFAULT_REGION=$TF_VAR_AWS_DEFAULT_REGION
 #v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
 #v="$v."
 #cp -r $v .
 unzip soln.zip
+
+export AWS_ACCESS_KEY_ID=$TF_VAR_AWS_ACCESS_KEY_ID
+export AWS_DEFAULT_REGION=$TF_VAR_AWS_DEFAULT_REGION
+export AWS_SECRET_ACCESS_KEY=$(cat access_key.pem)
