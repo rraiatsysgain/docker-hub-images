62f03d4593f487a3f3d37fa6bef20d463898acec
rraiatsysgain <rrai@sysgain.com>
2018-04-16 20:59:08 -0700

    fixed terraform logic

--- aws-terraform/install.sh
+++ aws-terraform/install.sh
@@ -2,18 +2,13 @@
 curl -o soln.zip $TEMPLATE_URL
 curl -o access_key.pem $TF_VAR_AWS_SECRET_ACCESS_KEY
 
-#v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
-#v="$v."
-#cp -r $v .
 unzip soln.zip
 
-#export AWS_ACCESS_KEY_ID=$TF_VAR_AWS_ACCESS_KEY_ID
-#export AWS_DEFAULT_REGION=$TF_VAR_AWS_DEFAULT_REGION
-#export AWS_SECRET_ACCESS_KEY=$(cat access_key.pem)
 cat <<EOF > aws_credential.tf
 provider "aws" {
-  region=$TF_VAR_AWS_DEFAULT_REGION
-  access_key=$TF_VAR_AWS_ACCESS_KEY_ID
-  secret_key=$TF_VAR_AWS_SECRET_ACCESS_KEY
+  region="$TF_VAR_AWS_DEFAULT_REGION"
+  access_key="$TF_VAR_AWS_ACCESS_KEY_ID"
+  secret_key="$TF_VAR_AWS_SECRET_ACCESS_KEY"
+  #secret_key="$(cat access_key.pem)"
 }
 EOF
--- azure-terraform/install.sh
+++ azure-terraform/install.sh
@@ -2,10 +2,15 @@
 curl -o soln.zip $TEMPLATE_URL
 curl -o app_secret.pem $AZ_VAR_app_secret
 
-#export ARM_TENANT_ID=$AZ_VAR_tenant_id
-#export ARM_CLIENT_ID=$AZ_VAR_app_id
-#export ARM_CLIENT_SECRET="$(cat app_secret.pem)"
-#v=$(unzip soln.zip | grep -m1 'creating:' | cut -d' ' -f5-)
-#v="$v."
-#cp -r $v .
 unzip soln.zip
+
+cat <<EOF > az_credential.tf
+provider "azurerm" {
+  #ARM_TENANT_ID="$AZ_VAR_tenant_id"
+  #ARM_CLIENT_ID="$AZ_VAR_app_id"  
+  #ARM_CLIENT_SECRET="$(cat app_secret.pem)"
+  tenant_id="$AZ_VAR_tenant_id"
+  client_id="$AZ_VAR_app_id"  
+  client_secret="$(cat app_secret.pem)"
+}
+EOF
