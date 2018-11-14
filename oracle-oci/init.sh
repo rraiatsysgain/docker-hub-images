41b81246de399a3a7adb2f12774d0ee373d2e3a3
rraiatsysgain <rrai@sysgain.com>
2018-11-14 11:11:52 -0800

    updated init

--- oracle-oci/init.sh
+++ oracle-oci/init.sh
@@ -1,5 +1,5 @@
-curl -o tf_remote_config.tf $TERR_REMOTE_STATE_URL
+curl -o tf_remote_config.tf $TERRAFORM_CONSUL_BACKEND_FILE
 ls
 pwd
 echo "Initializing terraform..."
-terraform init -input=false -backend-config="address=$TF_VAR_terraform_CONSUL_ADDRESS" -backend-config="path=$TF_VAR_deployment_auuid/terraform.tfstate" -backend-config="scheme=http" -force-copy
+terraform init -input=false -backend-config="address=$TERRAFORM_CONSUL_ADDRESS" -backend-config="path=$TF_VAR_deployment_auuid/terraform.tfstate" -backend-config="scheme=$TERRAFORM_CONSUL_SCHEME" -force-copy
